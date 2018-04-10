7e8f97ef06137ebf2a441eff7bafed34fb3fbd5a
Marc-André Brunelle-Langevin <Marc.andre.brunelle.langevin@hotmail.fr>
2018-04-10 14:24:23 -0400

    Début

---
+++ ConsoleApplication3/ConsoleApplication3/jeu.cpp
@@ -0,0 +1,9 @@
+#include "jeu.hpp"
+
+#include <math.h>
+#include "tableau.hpp"
+
+std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue) {
+  // Inserez votre code ici
+  return std::vector<Monstre>();
+}
---
+++ ConsoleApplication3/ConsoleApplication3/jeu.hpp
@@ -0,0 +1,10 @@
+#ifndef JEU
+#define JEU
+
+#include <vector>
+
+#include "monstre.hpp"
+
+std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue);
+
+#endif
