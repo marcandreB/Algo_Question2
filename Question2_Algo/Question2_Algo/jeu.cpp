d6c3353ba6ae7a4a4610684f347f53c57944398c
Marc-André Brunelle-Langevin <Marc.andre.brunelle.langevin@hotmail.fr>
2018-04-10 14:45:10 -0400

    calisse

---
+++ Question2_Algo/Question2_Algo/jeu.cpp
@@ -0,0 +1,12 @@
+#include "jeu.h"
+
+#include <math.h>
+#include "tableau.h"
+
+std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue) {
+	unsigned int dimensions[2] = { monstres.size(), magie_rouge };
+	Tableau<std::string> t(std::vector<unsigned int>(dimensions, dimensions + 2));
+
+
+	return std::vector<Monstre>();
+}
--- Question2_Algo/Question2_Algo/jeu.h
+++ Question2_Algo/Question2_Algo/jeu.h
@@ -1,1 +1,11 @@
+#ifndef JEU
+#define JEU
+
+#include <vector>
+
+#include "monstre.h"
+
+std::vector<Monstre> programmationDynamique(const std::vector<Monstre>& monstres, unsigned int magie_rouge, unsigned int magie_bleue);
+
+#endif
 #pragma once
